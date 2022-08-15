e769805c7d5d1e64f755a4e5db382209d353b6d1
ANDRII HALYTSKYI <galickyav@gmail.com>
2022-08-15 11:27:14 +0300

    дз-10-01

---
+++ src/fetchCountries.js
@@ -0,0 +1,6 @@
+export function fetchCountries(name){
+    return fetch(`https://restcountries.com/v3.1/name/${name}`)
+        .then(response => response.json())
+        .catch(error => console.log(error));
+}
+
